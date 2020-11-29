984580f960f78a9a7a34a9c7c72ff99e46c35044
JunCing <39450550+JunCing@users.noreply.github.com>
2020-11-29 11:12:45 +0800

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿const helloElem = document.querySelector("#hello");
+const url = "https://script.google.com/macros/s/AKfycbyBxHX0LQEqI1CwdEFCP2uDM37BnBebeVQzzRTQRKG4xD-tYwo/exec?name=Allen&age=29";
+
+fetch(url)
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(jsonData) {
+		helloElem.innerText = jsonData;
+	});
